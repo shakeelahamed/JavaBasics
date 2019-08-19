8dd15bf5c2cc0e70a1536a38ee5055a867fecd66
Shakeel Ahamed <sahamed@wsgc.com>
2019-08-19 15:08:51 -0700

    learing more

--- src/learn/java/basic/LearnString.java
+++ src/learn/java/basic/LearnString.java
@@ -10,5 +10,25 @@ public class LearnString {
 
         String textText = "Please find where locate is located!";
         System.out.println(textText.indexOf("locate"));
+
+        String firstName = "Shakeel";
+        String lastName = "Rahman";
+        System.out.println(firstName + " " + lastName);
+        System.out.println(firstName.concat(" ").concat(lastName));
+
+        String txt1 = "We are so called \"Vikings\" from the north.";
+        System.out.println(txt1);
+
+        String txt2 = "It\'s alright";
+        System.out.println(txt2);
+
+        System.out.println("This is before\fNow new line");
+        System.out.println("Textlksdlsfdks\rOverlap");
+        System.out.println("12\b3");
+
+        String x = "10";
+        int y = 20;
+        String z = x + y;
+        System.out.println(z);
     }
 }
