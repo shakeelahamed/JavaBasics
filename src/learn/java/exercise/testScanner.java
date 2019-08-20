56f194ddd217b9d700247e0851cdac13600f53c0
Shakeel Ahamed <sahamed@wsgc.com>
2019-08-20 10:25:44 -0700

    added scanner exercise

---
+++ src/learn/java/exercise/testScanner.java
@@ -0,0 +1,12 @@
+package learn.java.exercise;
+
+import java.util.Scanner;
+
+public class testScanner {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the input:");
+        int a = scanner.nextInt();
+        System.out.println(a);
+    }
+}
